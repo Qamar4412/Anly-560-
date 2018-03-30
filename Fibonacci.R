41a36dad51ed0ec00156737d5d74427a50350790
Qamar4412 <37852426+Qamar4412@users.noreply.github.com>
2018-03-30 19:11:48 -0400

    Implementation of Fibonacci R

---
+++ Fibonacci.R
@@ -0,0 +1,43 @@
+s (9 sloc) 173 Bytes 
+
+## fibonacci sequence
+
+
+
+
+
+
+
+len <- 10
+
+
+
+fibvals <- numeric(len)
+
+
+
+fibvals[1] <- 1
+
+
+
+fibvals[2] <- 1
+
+
+
+for (i in 3:len) { 
+
+
+
+  fibvals[i] <- fibvals[i-1]+fibvals[i-2]
+
+
+
+} 
+
+
+
+
+
+
+
+fibvals
